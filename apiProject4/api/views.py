480bd07a1611e26e4f5dfa4eca31c50cb5b9c810
manish ubnare <manishub75@gmail.com>
2026-02-17 16:20:29 +0530

    added apiProject4 with basic auth

--- apiProject4/api/views.py
+++ apiProject4/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+
+                    #    BASIC AUHENTICATION AND AllowAny AND IsAuthenticated
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+
+def public_view(request):
+    return Response({"message":"this is public api"})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+
+def private_view(request):
+    return Response(f"hello, {request.user.username}.this is private api")
--- apiProject4/apiProject4/settings.py
+++ apiProject4/apiProject4/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'rest_framework',
-    'api',
+     'api',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
